176899a20f9d082461506f84386a522283824a8c
prp <44913650+puripong1st@users.noreply.github.com>
2023-02-11 16:34:45 +0700

    1
    1

---
+++ msgup.py
@@ -0,0 +1,10 @@
+from tkinter import Canvas
+
+canvas.create_text(
+    240.0,
+    398.0,
+    anchor="nw",
+    text=f"ประกาศ {response}",
+    fill="#FF1E1E",
+    font=("Inter ExtraBold", 14 * -1)
+)
