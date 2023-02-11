a815b953b5d81504092a3e12fd5c5c45f78dbf9f
prp <44913650+puripong1st@users.noreply.github.com>
2023-02-11 16:57:27 +0700

    1
    1

--- msgup.py
+++ msgup.py
@@ -4,7 +4,7 @@ canvas.create_text(
     240.0,
     398.0,
     anchor="nw",
-    text=f"ประกาศ {response}",
+    text=f"ประกาศ นี้คือการเทสระบบดึงข้อมูลมาจากบนเว็บ",
     fill="#FF1E1E",
     font=("Inter ExtraBold", 14 * -1)
 )
